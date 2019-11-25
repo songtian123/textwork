32b3b17aeaa62470ee749c01abe8f8ae3da3f82f
宋甜 <@DESKTOP-3C0MTON>
2019-11-25 16:24:21 +0800

    ercu

--- src/git/USer.java
+++ src/git/USer.java
@@ -4,6 +4,10 @@ public class USer {
         private void sysou() {
 			// TODO Auto-generated method stub
                  System.out.println("1234567899");
+                 System.out.println("1234567899");
+                 System.out.println("1234567899");
+                 System.out.println("1234567899");
+                 
 		}
 
 }
